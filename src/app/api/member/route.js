b43518e491b0385b0154479d9b733e9abc4a3e28
Rudra-Lanote <langoterudra@gmail.com>
2024-09-22 16:39:15 +0530

    Started frontend

--- src/app/api/member/route.js
+++ src/app/api/member/route.js
@@ -11,7 +11,7 @@ export async function DELETE(req) {
         const { email } = body
         await Member.findOneAndDelete({ email });
         return NextResponse.json({
-            message : "Member removed"
+            message: "Member removed"
         }, {
             status: 200
         })
@@ -23,3 +23,58 @@ export async function DELETE(req) {
         })
     }
 }
+
+export async function POST(req) {
+    try {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const result = await Member.updateMany(
+            { endDate: { $gte: today, $lt: new Date(today.getTime() + 86400000) } },
+            { $set: { isMember: false } }
+        );
+
+        return NextResponse.json({
+            message: result
+        }, {
+            status: 200
+        })
+    } catch (error) {
+        return NextResponse.json({
+            error: error.message
+        }, {
+            status: 500
+        })
+    }
+
+
+}
+export async function PUT(req) {
+
+    try {
+        const body = req.json()
+        const { email, duration } = body
+        const dsd = Date.now() + (2592000000*duration)
+        await Member.updateOne({ email }, {
+            $set: {
+                isMember: true,
+                startDate: Date.now(),
+                endDate: dsd
+            }
+        })
+        return NextResponse.json({
+            message: "Membership renewed"
+        }, {
+            status: 200
+        })
+    } catch (error) {
+        return NextResponse.json({
+            error: error.message
+        }, {
+            status: 500
+        })
+    }
+
+
+}
+
